cd7e89ee5f621d906481447899dadd2102b653d4
camilleclaret <camille.claret@etud.univ-jfc.fr>
2019-09-24 14:47:04 +0200

    Insertion d'une classe Manche et de ses tests

---
+++ src/main/java/bowling/Manche.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bowling;
+
+/**
+ * Class ayant pour but le controle d'une manche normale on a min 9 manches a deux boules
+ * @author camilleclaret
+ */
+public class Manche {
+    
+    int numManche;
+    Manche mancheSuiv;
+    int boule1 = 0 ;
+    int boule2 = 0;
+    int bouleAct = 0 ;
+    
+    /*
+    Constructeurs de la classe
+    */
+    public Manche(){}
+    
+    public Manche(int num, Manche numSuiv){
+        if( num <1|| num > 9 ){
+            throw new IllegalArgumentException("Une manche normale peut être numéroté que de 1 à 9.");
+        } else if (numSuiv == null ){
+            throw new IllegalArgumentException("Une manche doit forcément avoir une suivante.");
+        } else {
+            this.numManche = num;
+            this.mancheSuiv = numSuiv;
+        }
+    }
+    
+    /*
+    Les fonctions suivante vont être pour la récupération des données
+    */
+    
+    public int getBouleAct(){
+        // retourn le numéro de la boule jouer 
+        return this.bouleAct;
+    }
+    
+    public int getNumManche(){
+        // retourne le numéro de manche 
+        return this.numManche;
+    }
+    
+    /*
+    Les fonction devant retourner un boolean 
+    */
+    
+    public boolean isFinManche(){
+        // La partie est fini si un joueur joue ses deux boules ou si il fait un stricke
+        return this.boule1 == 10 || this.bouleAct == 2;
+    }
+    
+    public boolean isStricke(){
+        // Le joueur fait un stricke si il décanille ses 10 quilles avec la première boule
+        return this.boule1 == 10;
+        
+    }
+    
+    public boolean isSpare(){
+        // Le joueur fait un spare si il decanille 10 quilles en 2 boules
+        return this.boule1 + this.boule2 == 10;
+    }
+    
+    /*
+    Calcul des bonus selon un spare ou un stricke
+    */
+    
+    public int stickeBonus(){
+       if(isStricke()){
+           return 10 + this.mancheSuiv.stickeBonus();
+       } else{
+           return boule1 + boule2;
+       }
+    }
+    
+    public int spareBonus(){
+        return 10 + this.mancheSuiv.boule1;
+    }
+    
+    /*
+    Calcul du score de la manche 
+    */
+    
+    public void quilleAbbatue(int nbrQ){
+        // Fonction qui affecte le nombre de quille abbatu par le joueur au bon numéro de boule
+        if(nbrQ > 10 || nbrQ <0){
+            throw new IllegalArgumentException("Un joueur peu abbattre entre 0 et 10 quille en un lancer");
+        } else if (nbrQ + this.boule1 > 10){
+            throw new IllegalArgumentException("En deux lancer le nbr maxi de quille abbatue est de 10");
+        } else {
+            this.bouleAct ++;
+            if (this.bouleAct == 1){
+                this.boule1 = nbrQ;
+            }else{
+                this.boule2 = nbrQ;
+            }
+        }
+    }
+    
+}
---
+++ src/test/java/bowling/MancheTest.java
@@ -0,0 +1,83 @@
+package bowling;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Fichier de test de la class Manche 
+ * @author camilleclaret
+ */
+public class MancheTest {
+    private Manche m;
+    private Manche m1;
+    
+    @Before
+    public void setUp() {
+        m1 = new Manche(2,new Manche());
+        m = new Manche(1,m1);
+    }
+    
+   @Test
+   public void mancheAcceptable(){
+       // vérification des exceptions lors de la création d'une manche
+       try{
+           m = new Manche(-1,m1);
+           m = new Manche(3,null);
+       }catch (IllegalArgumentException e){
+           assertEquals(1,m.getNumManche());
+       }
+   }
+   
+   @Test
+   public void get(){
+       // Vérification du numéro de manche 
+       assertEquals(1, m.getNumManche());
+       assertEquals(2,m1.getNumManche());
+       // Vérification de la boule jouer
+       m.quilleAbbatue(5);
+       assertEquals(1,m.getBouleAct());
+       m.quilleAbbatue(3);
+       assertEquals(2,m.getBouleAct());
+   }
+   
+   @Test
+   public void True(){
+       // Cas ou on attend un retour positif
+       m.quilleAbbatue(10);
+       assertTrue(m.isStricke());
+       assertTrue(m.isFinManche());
+       m1.quilleAbbatue(5);
+       m1.quilleAbbatue(5);
+       assertTrue(m1.isSpare());
+       assertTrue(m1.isFinManche());
+   }
+   
+   @Test
+   public void False(){
+       m.quilleAbbatue(5);
+       assertFalse(m.isStricke());
+       assertFalse(m.isFinManche());
+       m.quilleAbbatue(3);
+       assertFalse(m.isSpare());
+       
+   }
+   
+   @Test
+   public void BonnusStricke(){
+       m.quilleAbbatue(10);
+       m1.quilleAbbatue(3);
+       m1.quilleAbbatue(5);
+       assertEquals(18, m.stickeBonus());
+      
+   }
+   
+   @Test
+   public void BonnusSpare(){
+       m.quilleAbbatue(5);
+       m.quilleAbbatue(5);
+       m1.quilleAbbatue(10);
+       assertEquals(20, m.spareBonus());
+      
+   }
+}
